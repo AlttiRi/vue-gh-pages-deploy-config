6c41e7a67f370fe63d20aead616289dcd99ef371
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2023-01-07 23:52:12 +0300

    Add a comment in JSDoc

--- src/util.js
+++ src/util.js
@@ -1,4 +1,7 @@
-/** @return {string} */
+/**
+ * Returns random CSS color value as HSLA.
+ * @return {string}
+ */
 export function getRandomColor() {
     return "hsla(" + (Math.random() * 360) + ", 100%, 50%, 1)";
 }
